a00178f036c8afb4db44606f5d9ea1acf14cc92e
Shweta Tiwari <tiwarishweta2302@example.com>
2024-07-14 16:16:05 +0530

    Shwe

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,7 +2,7 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
-        B shw = new B();
-        System.out.println(shw.meth());
+        B shwe = new B();
+        System.out.println(shwe.meth());
     }
 }
