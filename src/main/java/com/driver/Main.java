f54d9c0123427e6f34277ed23de2959ded560dfd
Shweta Tiwari <tiwarishweta2302@example.com>
2024-07-16 17:24:50 +0530

    Shwe

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,6 +1,19 @@
 package com.driver;
 
 public class Main {
+    public static class A {
+        public String meth() {
+            return "Invoking method from class A";
+        }
+
+    }
+    public static class B extends A {
+        @Override
+        public String meth(){
+            return "Method is overridden in Extendend class B";
+        }
+
+    }
     public static void main(String[] args) {
         A shwe = new A();
         System.out.println(shwe.meth());
